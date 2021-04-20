f37d5298a6bf089574b2aee0f77306bdf51c7a31
康恆 徐 <107112221@mail.oit.edu.tw>
2021-04-20 14:59:16 +0800

    存檔

--- 109_2Mid/WebForm1.aspx
+++ 109_2Mid/WebForm1.aspx
@@ -11,9 +11,17 @@
     <form id="form1" runat="server">
         <div>
             <h1>會員註冊</h1>
-            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
-            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
-            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+            <asp:Label ID="Label1" runat="server" Text="會員帳號" Font-Size ="X-Large"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+                
+            <asp:Label ID="Label2" runat="server" Text="密碼" Font-Size= "X-Large"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+              
+            <asp:Label ID="Label3" runat="server" Text="興趣(單選)" Font-Size= "X-Large"></asp:Label>
+
+           
         </div>
     </form>
 </body>
--- 109_2Mid/WebForm1.aspx.designer.cs
+++ 109_2Mid/WebForm1.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace _109_2Mid
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Label1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// TextBox1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
         /// Label2 控制項。
         /// </summary>
         /// <remarks>
@@ -33,13 +51,13 @@ namespace _109_2Mid
         protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
-        /// Label1 控制項。
+        /// TextBox2 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
 
         /// <summary>
         /// Label3 控制項。
